2e55137af00e3fb5490f4c605531a6d06d509308
mizutanimeen <mizutanimeen@gmail.com>
2024-12-12 11:31:38 +0900

    a

--- README.md
+++ README.md
@@ -1,19 +1,30 @@
-### cliをコマンド実行可能状態
+### cliをコマンド実行可能状態(初回のみ)
 ```
 bash setup.sh
 ```
 
-### リソースグループ、container app 環境、container registryを作成、container registryにログイン
+### ログイン(初回以降)
+```
+az login
+```
+
+### リソースグループ、container app 環境
 ```
 bash start.sh
 ```
 
+### container registryを作成、container registryにログイン
+```
+bash container_start.sh
+```
+
 ### localのdocker imageを container registryにプッシュ
 ```
-bash image_push.sh
+bash image_push.sh image_name image_tag
 ```
 
 ### container appsを作成
+管理者アカウントをオンに
 ```
-
+bash container_app.sh container_app_name image_name:image_tag port registry_password
 ```
--- start.sh
+++ start.sh
@@ -18,24 +18,4 @@ az containerapp env create \
 
 echo "create container apps environment"
 
-# container registry の作成
-if [ -z "$CONTAINER_REGISTRY" ]; then
-  acrName=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w ${1:-32} | head -n 1 | cut -c 1-49)
-else
-  acrName=$CONTAINER_REGISTRY
-fi
-az acr create \
-  --resource-group $RESOURCE_GROUP \
-  --name $acrName \
-  --sku Basic \
-  --location $LOCATION
-
-echo "-----container registry name-----"
-echo $acrName
-echo "ACR_NAME=${acrName}" > acrName.txt
-echo "--------------------------------"
-echo "create container registry"
-
-az acr login --name $acrName
-
-echo "login container registry"
+echo "complete start.sh"
